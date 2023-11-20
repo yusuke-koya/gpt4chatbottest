b77fc510a697b8055433486492b6e6c2e4001725
yusuke-koya <yusuke-koya@exa-corp.co.jp>
2023-11-20 18:33:25 +0900

    output error log

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -62,6 +62,7 @@ const createCompletion = async (messages, context) => {
 };
 
 module.exports = async function (context, req) {
+  try{
   // Ignore retry requests
   if (req.headers["x-slack-retry-num"]) {
     context.log("Ignoring Retry request: " + req.headers["x-slack-retry-num"]);
@@ -187,6 +188,13 @@ module.exports = async function (context, req) {
   context.res = {
     status: 200,
   };
+  }catch(err){
+    context.log.error('****** error has occured ******');
+    context.log.error(err);
+    context.res = {
+      body: err
+    }
+  }
 };
 
 // NGワードが含まれるか
