816cbc9126e6d6d57160dcaeaccf4117b5fa7990
yusuke-koya <yusuke-koya@exa-corp.co.jp>
2023-12-04 13:14:27 +0900

    connect test

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -6,6 +6,40 @@ const {
   OpenAIApi,
 } = require("openai");
 
+
+const { OpenAIClient } = require("@azure/openai");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+async function main(){
+  const endpoint = "https://exa-dpf.openai.azure.com/";
+  const client = new OpenAIClient(endpoint, new DefaultAzureCredential());
+
+  const deploymentId = "gpt-35-turbo";
+
+  const messages = [
+    { role: "system", content: "You are a helpful assistant. You will talk like a pirate." },
+    { role: "user", content: "Can you help me?" },
+    { role: "assistant", content: "Arrrr! Of course, me hearty! What can I do for ye?" },
+    { role: "user", content: "What's the best way to train a parrot?" },
+  ];
+
+  console.log(`Messages: ${messages.map((m) => m.content).join("\n")}`);
+
+  const events = client.listChatCompletions(deploymentId, messages, { maxTokens: 128 });
+  for await (const event of events) {
+    for (const choice of event.choices) {
+      const delta = choice.delta?.content;
+      if (delta !== undefined) {
+        console.log(`Chatbot: ${delta}`);
+      }
+    }
+  }
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
+
 const openaiClient = new OpenAIApi(
   new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
